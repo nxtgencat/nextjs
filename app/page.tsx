a68b0df4aa9c5ccc4528e98ada8cb0c1ad26f29c
Naveen <naveenreddypasupula@gmail.com>
2025-11-23 18:09:37 +0530

    added slider to show the content

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import ServicesCards from "../components/services";
 import ServicesBottom from "../components/servicesBottom";
+import SkinTreatmentSlider from "../components/sliders";
 export default function Home() {
   return (
     <>
     <ServicesCards/>
     <ServicesBottom/>
+    <SkinTreatmentSlider />
     </>
   )
 }
